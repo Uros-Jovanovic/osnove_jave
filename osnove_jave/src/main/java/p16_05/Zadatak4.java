014daaba1db11982f39978594d069e4c4e152e88
Uros Jovanovic <woxnn@live.com>
2022-05-25 16:51:19 +0200

    asdag

---
+++ osnove_jave/src/main/java/p16_05/Zadatak4.java
@@ -0,0 +1,38 @@
+package p16_05;
+
+public class Zadatak4 {
+    public static void main(String[] args) {
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        System.out.println("");
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        System.out.println("");
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        System.out.println("");
+        zvezdica();
+        zvezdica();
+        zvezdica();
+        System.out.println("");
+        zvezdica();
+        zvezdica();
+        System.out.println("");
+        zvezdica();
+        System.out.println("");
+
+    }
+
+    static void zvezdica() {
+        System.out.print("* ");
+    }
+}
