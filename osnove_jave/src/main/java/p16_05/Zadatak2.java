56a9ccbe0c2057f1ff581bffc12f687e82facba8
Uros Jovanovic <woxnn@live.com>
2022-05-25 16:49:50 +0200

    asdag git pull

---
+++ osnove_jave/src/main/java/p16_05/Zadatak2.java
@@ -0,0 +1,29 @@
+package p16_05;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+//        Zadatak
+//        Napisati dve metode, jedna metoda sluzi za stampanje
+//        imena a druga sluzi za stampanje prezimena.
+//       U glavnom programu pozvati metode na izvrsenje tako da se 5 puta
+//       ispise ime i prezime u istom redu.
+//        Primer izvrsenja:
+//        Milan Jovanovic
+//        Milan Jovanovic
+//        Milan Jovanovic
+//        Milan Jovanovic
+//        Milan Jovanovic
+
+        ime();prezime();
+        ime();prezime();
+        ime();prezime();
+        ime();prezime();
+        ime();prezime();
+
+
+    }
+    static void ime() {
+        System.out.print("Uros"); }
+    static void prezime() {
+        System.out.println(" Jovanovic"); }
+}
