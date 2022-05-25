8290d37307bace45d6b9c8d8a8ddf62f150aaaf7
Uros Jovanovic <woxnn@live.com>
2022-05-25 17:12:32 +0200

    Domaci 13.05.2022. - Zadatak1

--- osnove_jave/src/main/java/d13_05/Zadatak1.java
+++ osnove_jave/src/main/java/d13_05/Zadatak1.java
@@ -7,24 +7,6 @@ public class Zadatak1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         ArrayList<Integer> nizA = new ArrayList<Integer>();
-
-        //        Kreirati novi fajl u paketu sa danasnjeg predavanja.
-//        Fajl se zove tekstovi_zadataka.txt i u njemu pejstujte tekst sa ovog zadataka.
-//                Kreirati novu java klasu Git_Zadatak1.java i u njoj
-//        ispisite na ekranu sve git naredbe koje smo radili.
-//                Commit-ujte ovo kao promenu “Predavanja 13.05 Git Zadatak 1”
-//        Push-ujte na github
-//        git status
-//        git add osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
-//        git add osnove_jave/src/main/java/p13_05_2022/tekstovi_zadataka.txt
-//        git commit -m “Predavanja 13.05 Git Zadatak 1”
-//        git push
-//
-//
-//        zamena za vise git add-ova
-//        git add . => sve promene baci u stage
-
-
         nizA.add(3);
         nizA.add(5);
         nizA.add(2);
