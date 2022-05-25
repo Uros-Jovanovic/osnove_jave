37054f9e55870323dde3161ea7bb7867f5f0aa5a
Uros Jovanovic <woxnn@live.com>
2022-05-25 16:56:16 +0200

    fhdggdfsf

---
+++ osnove_jave/src/main/java/p23_05_Vezbanje/Video.java
@@ -0,0 +1,61 @@
+//package p23_05_Vezbanje;
+//
+//public class Video {
+//    private String id;
+//    private String naziv;
+//    private int duzina;
+//    private int brojLajkova;
+//    private int brojDislajkova;
+//    private int brojPregleda;
+//
+//    public void setYoutubePlayer(YoutubePlayer youtubePlayer) {
+//        this.youtubePlayer = youtubePlayer;
+//    }
+//
+//    private YoutubePlayer youtubePlayer;
+//
+//    public YoutubePlayer getYoutubePlayer() {
+//        return youtubePlayer;
+//    }
+//
+//    public void lajkuj(String like) {
+//        this.brojLajkova = brojLajkova + 1;
+//    }
+//
+//    public void dislajkuj(String dislike) {
+//        this.brojDislajkova = brojDislajkova - 1;
+//    }
+//
+//    public void pogledaj(String watch) {
+//        this.brojPregleda = brojPregleda + 1;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public String getNaziv() {
+//        return naziv;
+//    }
+//
+//    public int getDuzina() {
+//        return duzina;
+//    }
+//
+//    public int getBrojLajkova() {
+//        return brojLajkova;
+//    }
+//
+//    public int getBrojDislajkova() {
+//        return brojDislajkova;
+//    }
+//
+//    public int getBrojPregleda() {
+//        return brojPregleda;
+//    }
+//
+//    public void ucitajVideo(Video video){
+//        brojPregleda = this.getBrojPregleda() + 1;
+//        trenutnoVreme =
+//    }
+//}
---
+++ osnove_jave/src/main/java/p23_05_Vezbanje/YoutubePlayer.java
@@ -0,0 +1,15 @@
+//package p23_05_Vezbanje;
+//
+//public class YoutubePlayer {
+//
+//    private int kvalitet;
+//    private String rezim;
+//    private int jacina;
+//    private int trenutnoVreme;
+//
+//    public YoutubePlayer() {
+//        this.jacina = 75;
+//        this.kvalitet = 144;
+//        this.rezim = "mini player";
+//    }
+//}
