29b5f098baa72b06d7397793b2088bbd36ce794e
Uros Jovanovic <woxnn@live.com>
2022-05-25 16:47:49 +0200

    asdad

---
+++ osnove_jave/src/main/java/p16_05/Zadatak1.java
@@ -0,0 +1,19 @@
+package p16_05;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+//        Zadatrak
+//        Napisati metodu koja stampa
+//        ime i prezime i u glavnom programu pozvati metodu na izvrsenje 5 puta.
+        imeiprezime();
+        imeiprezime();
+        imeiprezime();
+        imeiprezime();
+        imeiprezime();
+    }
+
+
+    static void imeiprezime() {
+        System.out.println("Uros Jovanovic");
+    }
+}
