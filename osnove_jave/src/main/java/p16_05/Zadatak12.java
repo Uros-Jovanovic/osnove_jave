255a12edddbe0e3be0501dda8d2857ad14d2acae
Uros Jovanovic <woxnn@live.com>
2022-05-25 16:49:11 +0200

    asdada

---
+++ osnove_jave/src/main/java/p16_05/Zadatak12.java
@@ -0,0 +1,12 @@
+//package p16_05;
+//
+//public class Zadatak12 {
+//    public static void main(String[] args) {
+//
+//    }
+//        static double valuta(String valuta1, String valuta2, int koliko) {
+//            if (broj.equals("EUR")) {
+//                return 1;
+//    }
+//}
+//}
