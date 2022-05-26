85a1a44f606bbfa2f86e2d3a35ca6b7ba32a7871
Uros Jovanovic <woxnn@live.com>
2022-05-26 02:15:10 +0200

    Domaci 16.05.2022. - Zadatak 2

--- osnove_jave/src/main/java/d16_05/Zadatak2.java
+++ osnove_jave/src/main/java/d16_05/Zadatak2.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Zadatak2 {
     public static void main(String[] args) {
-//        Napisati funkciju koja za dva jednocifrena broja
-//        koja su ulazni parametri funkcije vraca novu vrednost koja se formira kao na primeru:
-//        ako je prvi broj =2, a drugi =3 vraca novu vrednost 23.
-//        ako je prvi broj =6, a drugi =2 vraca novu vrednost 62.
+//       Napisati funkciju koja za dva jednocifrena broja
+//       koja su ulazni parametri funkcije vraca novu vrednost koja se formira kao na primeru:
+//       ako je prvi broj =2, a drugi =3 vraca novu vrednost 23.
+//       ako je prvi broj =6, a drugi =2 vraca novu vrednost 62.
         Scanner s = new Scanner(System.in);
         System.out.print("Unesite vrednost x: ");
         int x = s.nextInt();
