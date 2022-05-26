3a4532045fa5643d4febc711b793153400d18de9
Uros Jovanovic <woxnn@live.com>
2022-05-26 02:08:47 +0200

    Domaci 16.05.2022. Zadatak 1

--- osnove_jave/src/main/java/d16_05/Zadatak1.java
+++ osnove_jave/src/main/java/d16_05/Zadatak1.java
@@ -2,9 +2,9 @@ package d16_05;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-//        Napisati metodu stampajVrednostZa10Vecu
-//        u kojoj se stampa prosledjena vrednost za 10 veca.
-//            Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
+//       Napisati metodu stampajVrednostZa10Vecu
+//       u kojoj se stampa prosledjena vrednost za 10 veca.
+//       Iz main-a pozvati izvrsenje metode za razlicite vrednosti.
         stampajVrednostZa10Vecu(20);
 
     }
