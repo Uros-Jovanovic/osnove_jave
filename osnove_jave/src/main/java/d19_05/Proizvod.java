2a6de576f24a26a6cb4b108499c4abcc203356e1
Uros Jovanovic <woxnn@live.com>
2022-05-26 05:01:20 +0200

    Domaci 19.05.2022. - Zadatak 1 - Proizvod

--- osnove_jave/src/main/java/d19_05/Proizvod.java
+++ osnove_jave/src/main/java/d19_05/Proizvod.java
@@ -5,6 +5,15 @@ public class Proizvod {
     public double cenaProizvoda;
     public double tezinaProizvoda;
 
+    public Proizvod() {
+    }
+
+    public Proizvod(String nazivProizvoda, double cenaProizvoda, double tezinaProizvoda) {
+        this.nazivProizvoda = nazivProizvoda;
+        this.cenaProizvoda = cenaProizvoda;
+        this.tezinaProizvoda = tezinaProizvoda;
+    }
+
     public void stampaj() {
         System.out.println(nazivProizvoda + ", " + cenaProizvoda + " dinara, " + tezinaProizvoda + "grama.");
     }
@@ -13,9 +22,9 @@ public class Proizvod {
         this.cenaProizvoda = this.cenaProizvoda + poskupljenje;
     }
 
-    public double smanjenje(double popust) {
-        return this.cenaProizvoda - this.cenaProizvoda * popust / 100;
-    }
+    public double vratiCenuSaPopustom(double popust) {
+        double cenaSaPopustom = this.cenaProizvoda - this.cenaProizvoda * popust / 100;
+    return  cenaSaPopustom;}
 
     public int postarina() {
         if (this.tezinaProizvoda <= 100) {
