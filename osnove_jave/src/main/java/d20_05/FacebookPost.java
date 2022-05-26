0118f0fd863350bd28f3fe15529b600b108eed33
Uros Jovanovic <woxnn@live.com>
2022-05-26 16:50:08 +0200

    Domaci 20.05.2022. - Zadatak 1 - FacebookPost

---
+++ osnove_jave/src/main/java/d20_05/FacebookPost.java
@@ -0,0 +1,42 @@
+package d20_05;
+
+public class FacebookPost {
+//    .Kreirati klasu FacebookPost koja ima:
+//    Od atributa:
+//    ime i prezime korisnika koji je objavio post
+//    ime i prezime korisnika na kom je profilu objavljen post
+//            (posto na fejsbuku mozete da postavite objavu na tudjem profilu)
+//    tekst objave
+//    broj lajkova
+//    broj deljenja
+ public String objavio;
+ public String profil;
+ public String tekst;
+ public int likes;
+ public int shares;
+
+    public FacebookPost() {
+    }
+
+    public FacebookPost(String objavio, String profil, String tekst) {
+        this.objavio = objavio;
+        this.profil = profil;
+        this.tekst = tekst;
+    }
+    public void lajkuj(){
+        this.likes = this.likes + 1;}
+    public void dislajkuj() {
+        if (this.likes > 0) {
+        this.likes = this.likes - 1;}
+        else { this.likes = 0;}
+    }
+    public void sheruj(){
+        this.shares++;
+    }
+    public void stampaj(){
+        System.out.println(objavio + " >>> " + profil);
+        System.out.println(tekst);
+        System.out.println("Likes: " + this.likes + " |  Shares: " + this.shares);
+    }
+}
+
---
+++ osnove_jave/src/main/java/d20_05/FacebookPostMain.java
@@ -0,0 +1,31 @@
+package d20_05;
+
+public class FacebookPostMain {
+    public static void main(String[] args) {
+        FacebookPost newPost1 = new FacebookPost("Uros Jovanovic", "Nikola Nikolic", "Sta se radi?");
+        newPost1.lajkuj();
+        newPost1.sheruj();
+        newPost1.lajkuj();
+        newPost1.dislajkuj();
+        newPost1.lajkuj();
+        newPost1.lajkuj();
+        newPost1.sheruj();
+        newPost1.sheruj();
+        newPost1.sheruj();
+
+        newPost1.stampaj();
+        System.out.println();
+        FacebookPost newPost2 = new FacebookPost("Petar Petrovic", "Tijana Tijanic", "Pozdrav");
+        newPost2.lajkuj();
+        newPost2.lajkuj();
+        newPost2.lajkuj();
+        newPost2.lajkuj();
+        newPost2.lajkuj();
+        newPost2.sheruj();
+        newPost2.sheruj();
+        newPost2.sheruj();
+        newPost2.stampaj();
+
+
+    }
+}
