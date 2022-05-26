380bf266d13469a2ee7a7d22a7c50f448f3457c6
Uros Jovanovic <woxnn@live.com>
2022-05-26 15:06:06 +0200

    Predavanja 16.05.2022.

--- osnove_jave/src/main/java/p16_05/Zadatak8.java
+++ osnove_jave/src/main/java/p16_05/Zadatak8.java
@@ -1,12 +1,27 @@
-//package p16_05;
-//
-//public class Zadatak8 {
-//    public static void main(String[] args) {
-//        zvezdica();
-//
-//    }
-//    static void zvezdica(int brojZvezda) {
-//        for (int i = 0; i < brojZvezda; i++) {
-//            System.out.print("* ");
-//}
-//    }}
+package p16_05;
+
+import java.util.Scanner;
+
+public class Zadatak8 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Unesite pocetni broj zvezdica: ");
+        int x = s.nextInt();
+        int b = x -1 ;
+
+        for (int i = 0; i < x && b!=-1; i++) {
+            if (i < b ) {
+                zvezdica(); }
+            else { zvezdica();
+
+                System.out.println("");
+                i=-1;
+                b--;}
+        }
+    }
+
+
+    static void zvezdica() {
+        System.out.print("* ");
+    }
+}
