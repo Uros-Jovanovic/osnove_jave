907bd1963c408e4137a2559302f8a043c8d14f36
Uros Jovanovic <woxnn@live.com>
2022-05-26 05:02:09 +0200

    Domaci 19.05.2022. - Zadatak 1 - Main

--- osnove_jave/src/main/java/d19_05/Zadatak1.java
+++ osnove_jave/src/main/java/d19_05/Zadatak1.java
@@ -3,7 +3,7 @@ package d19_05;
 public class Zadatak1 {
     public static void main(String[] args) {
         Proizvod proizvodPrvi = new Proizvod();
-       proizvodPrvi.nazivProizvoda = "Cigarete";
+        proizvodPrvi.nazivProizvoda = "Cigarete";
         proizvodPrvi.cenaProizvoda = 340.00;
         proizvodPrvi.tezinaProizvoda = 50.00;
 
@@ -12,23 +12,14 @@ public class Zadatak1 {
         proizvodDrugi.cenaProizvoda = 130.00;
         proizvodDrugi.tezinaProizvoda = 200.00;
 
+        double novaCena = proizvodPrvi.vratiCenuSaPopustom(20);
 
         proizvodPrvi.stampaj();
-        System.out.println();
         proizvodDrugi.stampaj();
-     System.out.println();
-
         proizvodPrvi.povecanje(40);
-        proizvodDrugi.smanjenje(30);
-
-        proizvodPrvi.stampaj();
         System.out.println();
-        proizvodDrugi.stampaj();
-     System.out.println();
-
-
-     System.out.println("Cena sa popustom za prvi proizvod je " + proizvodPrvi.smanjenje(30) + " dinara.");
-        System.out.println("Cena sa popustom za drugi proizvod je " + proizvodDrugi.smanjenje(30)+ " dinara.");
+        proizvodPrvi.stampaj();
+        System.out.println("Cena sa popustom je " + novaCena);
         System.out.println();
         System.out.println("Postarina za prvi proizvod je " + proizvodPrvi.postarina());
         System.out.println("Postarina za drugi proizvod je " + proizvodDrugi.postarina());
